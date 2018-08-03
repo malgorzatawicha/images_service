72f85911fb008fa949264b762ebb2601bdf13c5f
Małgorzata Wicha <mwicha@olx.pl>
2018-08-03 17:45:58 +0200

    basic_infrastructure: Create Private and Public Subnet

--- infrastructure/ec2.tf
+++ infrastructure/ec2.tf
@@ -1,6 +1,26 @@
 resource "aws_vpc" "images_vpc" {
   cidr_block = "10.0.0.0/16"
   tags {
-    Name = "images_vpc"
+    Name = "Images Vpc"
+  }
+}
+
+resource "aws_subnet" "images_public_subnet" {
+  cidr_block = "10.0.1.0/24"
+  vpc_id = "${aws_vpc.images_vpc.id}"
+  availability_zone = "eu-west-1a"
+
+  tags {
+    Name = "Images Public Subnet"
+  }
+}
+
+resource "aws_subnet" "images_private_subnet" {
+  cidr_block = "10.0.2.0/24"
+  vpc_id = "${aws_vpc.images_vpc.id}"
+  availability_zone = "eu-west-1b"
+
+  tags {
+    Name = "Images Private Subnet"
   }
 }
