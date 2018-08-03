e0dd88935c2706501f029d30457fb4e8ce9d61cf
Małgorzata Wicha <mwicha@olx.pl>
2018-08-03 17:54:32 +0200

    basic_infrastructure: Make Public Subnet accessible from internet

--- infrastructure/ec2.tf
+++ infrastructure/ec2.tf
@@ -24,3 +24,28 @@ resource "aws_subnet" "images_private_subnet" {
     Name = "Images Private Subnet"
   }
 }
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = "${aws_vpc.images_vpc.id}"
+  tags {
+    Name = "Images Internet Gateway"
+  }
+}
+
+resource "aws_route_table" "images_public_rt" {
+  vpc_id = "${aws_vpc.images_vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.internet_gateway.id}"
+  }
+
+  tags {
+    Name = "Images Public Subnet RT"
+  }
+}
+
+resource "aws_route_table_association" "images_public_rt" {
+  route_table_id = "${aws_route_table.images_public_rt.id}"
+  subnet_id = "${aws_subnet.images_public_subnet.id}"
+}
