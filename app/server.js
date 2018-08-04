70706f7730a29a6810e7f9bf8ba1ec7267a4787f
Małgorzata Wicha <mwicha@olx.pl>
2018-08-04 17:26:58 +0200

    Simple Node.js and DynamoDB integration

--- app/server.js
+++ app/server.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const express = require('express');
+const AWS = require('aws-sdk');
+AWS.config.update({region: 'eu-west-1'});
 
 // Constants
 const PORT = 80;
@@ -8,8 +10,23 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-    res.send('Hello world\n');
+app.get('/', (request, response) => {
+
+    const database = new AWS.DynamoDB({apiVersion: '2012-10-08'});
+    const params = {
+        TableName: 'db_images',
+        Key: {
+            'imageId' : {S: '5'},
+        }
+    };
+
+    database.getItem(params, function(error, data) {
+        if (error) {
+            response.send(error);
+        } else {
+            response.send(data.Item);
+        }
+    });
 });
 
 app.listen(PORT, HOST);
