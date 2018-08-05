ddab60979e08e8693cb412c9d5e9f122273a6398
Małgorzata Wicha <mwicha@olx.pl>
2018-08-05 15:02:37 +0200

    Getting all images from database in images endpoint

--- app/server.js
+++ app/server.js
@@ -27,68 +27,26 @@ app.use(bodyParser.json());
 const router = express.Router();
 
 router.get('/', (request, response) => {
-    const params = {
-        TableName: config.aws_table_name
-    };
-    database.scan(params, function(error, data) {
-        if (error) {
-            console.log(error);
-            return responseInternalServerError(response);
-        }
-        const { Items } = data;
-        return response.send({
-            data: Items
-        });
-    });
+    return response.status(303).append("Location", "/v1/images/").send();
 });
 
 // @todo add pagination to request
 
 router.route('/images')
     .get(function (request, response) {
-        response.json({
-            data: [
-                {
-                    id: 1,
-                    url: "www.google.com/image1",
-                    imageName: "custom name",
-                    original: {
-                        width: 1200,
-                        height: 900,
-                        url: "www.amazon.com/s3/image1/full"
-                    },
-                    width50: {
-                        width: 50,
-                        height: 40,
-                        url: "www.amazon.com/s3/image1/50"
-                    },
-                    width100: {
-                        width: 100,
-                        height: 80,
-                        url: "www.amazon.com/s3/image1/100"
-                    }
-                },
-                {
-                    id: 2,
-                    url: "www.google.com/image2",
-                    imageName: "second custom name",
-                    original: {
-                        width: 700,
-                        height: 700,
-                        url: "www.amazon.com/s3/image2/full"
-                    },
-                    width70: {
-                        width: 70,
-                        height: 70,
-                        url: "www.amazon.com/s3/image2/70"
-                    },
-                    width140: {
-                        width: 140,
-                        height: 140,
-                        url: "www.amazon.com/s3/image1/140"
-                    }
-                }
-            ]
+
+        const params = {
+            TableName: config.aws_table_name
+        };
+        database.scan(params, function(error, data) {
+            if (error) {
+                console.log(error);
+                return responseInternalServerError(response);
+            }
+            const { Items } = data;
+            return response.send({
+                data: Items
+            });
         });
     })
 
