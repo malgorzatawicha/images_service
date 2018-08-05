47a60a05178efd3ab9bd1a6e67936fabd2fd6c12
Małgorzata Wicha <mwicha@olx.pl>
2018-08-05 10:12:47 +0200

    Implement skeleton of list images

--- app/server.js
+++ app/server.js
@@ -29,5 +29,52 @@ app.get('/', (request, response) => {
     });
 });
 
+app.get('/v1/images', function (request, response) {
+    response.json({
+        data: [
+            {
+                id: 1,
+                url: "www.google.com/image1",
+                imageName: "custom name",
+                original: {
+                    width: 1200,
+                    height: 900,
+                    url: "www.amazon.com/s3/image1/full"
+                },
+                width50: {
+                    width: 50,
+                    height: 40,
+                    url: "www.amazon.com/s3/image1/50"
+                },
+                width100: {
+                    width: 100,
+                    height: 80,
+                    url: "www.amazon.com/s3/image1/100"
+                }
+            },
+            {
+                id: 2,
+                url: "www.google.com/image2",
+                imageName: "second custom name",
+                original: {
+                    width: 700,
+                    height: 700,
+                    url: "www.amazon.com/s3/image2/full"
+                },
+                width70: {
+                    width: 70,
+                    height: 70,
+                    url: "www.amazon.com/s3/image2/70"
+                },
+                width140: {
+                    width: 140,
+                    height: 140,
+                    url: "www.amazon.com/s3/image1/140"
+                }
+            }
+        ]
+    });
+});
+
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
