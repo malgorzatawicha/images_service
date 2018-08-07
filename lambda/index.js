9903fea06c17193cc6472ba80925cea2d0049f5c
Małgorzata Wicha <mwicha@olx.pl>
2018-08-07 20:27:34 +0200

    save row status in database

--- lambda/index.js
+++ lambda/index.js
@@ -4,6 +4,8 @@ const sharp = require('sharp');
 const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
 
 const s3 = new AWS.S3();
+const database = new AWS.DynamoDB.DocumentClient();
+
 
 const tableName = 'db_images';
 const bucket = 'mwicha-s3-bucket';
@@ -24,23 +26,60 @@ exports.handler = function(events, context, callback) {
     callback(null);
 };
 
+
 const insertRowListener = function (row) {
     const url = row.imageUrl.S;
     const id = row.imageId.S;
     const sizes = row.sizes.L;
 
+    const images = {};
+
     return downloadFileFromExternalResource(url)
         .then((buffer) => {
-            let sequence = saveToS3Promise(buffer, id + "/original");
+
+
+            let sequence = saveToS3Promise(buffer, id + "/original")
+                .then(findSizePromise(buffer))
+                .then((metadata) => {
+                    images.original = {
+                        width: metadata.width,
+                        height: metadata.height,
+                        url: "http://s3.amazonaws.com/" + bucket + "/" + id + "/original"
+                    };
+                })
+            ;
 
             return sizes.reduce((sequence, size) => {
 
                 return sequence.then(changeSizePromise(buffer, size.S))
                     .then(({data, info}) => {
+                        images['width' + size.S] = {
+                            url: "http://s3.amazonaws.com/" + bucket + "/" + id + "/width" + size.S,
+                            width: info.width,
+                            height: info.height
+                        };
                         return saveToS3Promise(data, id + '/width' + size.S);
                     })
-
             }, sequence);
+        }).then(() => {
+            const params = {
+                TableName: tableName,
+                Key: {
+                    "imageId": id
+                },
+                UpdateExpression: "set images=:images, imageStatus=:status",
+                ConditionExpression: "attribute_exists(imageId)",
+                ExpressionAttributeValues: {
+                    ":images": images,
+                    ":status": 'active'
+                }
+            };
+
+            database.update(params, function (error, data) {
+                if (error) {
+                    console.log(error);
+                }
+            });
         });
 };
 
@@ -56,7 +95,6 @@ const downloadFileFromExternalResource = function (url) {
 };
 
 const saveToS3Promise = (buffer, key) => {
-    console.log(buffer);
     return s3.putObject({
         Bucket: bucket,
         Key: key,
@@ -68,4 +106,8 @@ const saveToS3Promise = (buffer, key) => {
 const changeSizePromise = (buffer, size) => {
   return () => sharp(buffer).resize(parseInt(size), null)
       .toBuffer({ resolveWithObject: true });
+};
+
+const findSizePromise = (buffer) => {
+    return () => sharp(buffer).metadata()
 };
